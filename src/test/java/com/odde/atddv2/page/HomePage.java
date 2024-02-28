615305dd89e20cdaf41db7a49f71dea101ee7c86
Allen Lee <allen.lee@linecorp.com>
2024-02-28 15:22:48 +0800

    refactor: remove unused method

--- src/test/java/com/odde/atddv2/page/HomePage.java
+++ src/test/java/com/odde/atddv2/page/HomePage.java
@@ -10,10 +10,6 @@ public class HomePage {
     @Autowired
     private Browser browser;
 
-    public void open() {
-        browser.launch();
-    }
-
     public void login(String userName, String password) {
         browser.inputByPlaceholder("用户名", userName);
         browser.inputByPlaceholder("密码", password);
